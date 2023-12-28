913dd18f1029a9a1ea521977997f9a8468ebe322
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2023-12-29 00:42:12 +0530

    updated vercel file

--- src/server.js
+++ src/server.js
@@ -34,4 +34,4 @@ app.get("/",function(req,res){
 });
 
 const PORT = 5000;
-app.listen(PORT,()=> console.log(`Server started on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
